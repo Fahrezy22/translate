baeaf446240ff25cb1deb0d6e3a00ba485f592c4
Fahrezy22 <fahriza2222@gmail.com>
2022-04-27 22:22:27 +0800

    fix all

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -89,18 +89,10 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
               <li class="nav-item">
-                <a href="#" class="nav-link {{ request()->is('dashboard') ? 'active' : ''}}">
+                <a href="#" class="nav-link active">
                   <i class="nav-icon fas fa-tachometer-alt"></i>
                   <p>
                     Dashboard
-                  </p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="{{route('sentence.index')}}" class="nav-link {{ request()->is('sentence') ? 'active' : ''}}">
-                  <i class="nav-icon fas fa-tachometer-alt"></i>
-                  <p>
-                    Kata
                   </p>
                 </a>
               </li>
